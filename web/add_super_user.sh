6730aaa0efb99fa6e8ec394d6b47a07f82ae112e
hiroyuki naito <naito.hiroyuki@nims.go.jp>
2019-02-08 10:08:24 -0800

    Changed Dockerfile script for the automated superuseradd to Django

--- web/add_super_user.sh
+++ web/add_super_user.sh
@@ -1,1 +1,1 @@
-python manage.py createsuperuser
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('mgi_superuser', 'user_email@institution.com', 'mgi_superuser_pwd')" | python manage.py shell
--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -16,7 +16,6 @@ echo "  ---------------------Start celery-----------------------"
 rm -f *.pid
 celery multi start -A mdcs worker -l info -Ofair --purge
 chmod 777 ./worker.log
-su -m myuser -c "celery multi start -A mdcs worker -l info -Ofair --purge"
 
 # Wait for celery
 until celery -A mdcs status 2>/dev/null; do
@@ -30,10 +29,12 @@ python manage.py migrate
 #echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | ./manage.py shell
 # Collectstatic
 python manage.py collectstatic --noinput
+
+# Superuser Create
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('mgi_superuser', 'user_email@institution.com', 'mgi_superuser_pwd')" | python manage.py shell
     
 # Start Django
 echo "  ----------------------Start Django-----------------------"
 # uwsgi --socket mysite.sock --chdir /srv/mgi-mdcs/ --wsgi-file /srv/mgi-mdcs/mdcs/wsgi.py --chmod-socket=666
-# uwsgi --wsgi-file /srv/mgi-mdcs/mdcs/wsgi.py --static-map /static=./static --http 0.0.0.0:8000
 python manage.py runserver 0.0.0.0:8000
 echo Started
