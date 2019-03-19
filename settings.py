09042c96442afbc050eec9fac40f95ab09513cb9
hiroyuki naito <naito.hiroyuki@nims.go.jp>
2019-03-19 01:40:11 -0700

    Add postgres image on docker-compose.yml and delete redis folder. Add redis-volume and psql-volume on docker-compose.yml

--- settings.py
+++ settings.py
@@ -119,12 +119,23 @@ WSGI_APPLICATION = 'nmrr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'USER':"mgi_user",
+          'PASSWORD': "mgi_password",
+          'NAME': 'mgi',
+          'HOST': 'psql',
+          'PORT': 5432,
+      }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
